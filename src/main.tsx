705e26d02e44ffb90a05746f42cd4def519d0f8b
Aleksandr <ar@synerg.com>
2025-05-26 18:11:01 -0400

    clean-up: remove unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { restaurants } from "./mocks/restaurants.mock";
 
-import { Header } from "./components/Header/Header";
-import { Footer } from "./components/Footer/Footer";
 import "./styles.css";
-import { Tab } from "./components/Tab/Tab";
-import { StrictMode, useState } from "react";
-import { RestaurantComponent } from "./components/Restaurant/Restaurant";
+import { StrictMode } from "react";
 import { Layout } from "./components/Layout/Layout";
 import { Restaurants } from "./components/Restaurants/Restaurants";
 
