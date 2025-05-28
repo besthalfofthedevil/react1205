2e1ddc1dd376a778254f720969897b732ab3ac17
Aleksandr <ar@synerg.com>
2025-05-28 17:40:35 -0400

    Style counter module

--- src/components/Counter/counter.tsx
+++ src/components/Counter/counter.tsx
@@ -1,4 +1,6 @@
 import type { JSX } from "react";
+import styles from "./counter.module.css";
+import classNames from "classnames";
 
 export const Counter = ({
   count,
@@ -10,10 +12,18 @@ export const Counter = ({
   substract: (e: any) => void;
 }): JSX.Element => {
   return (
-    <div>
-      <button onClick={substract}>-</button>
-      <span>{count}</span>
-      <button onClick={add}>+</button>
+    <div className={classNames({[styles.quantityControl]: true})}>
+      <button className={styles.quantityBtn} onClick={substract}>
+        -
+      </button>
+      <span className={styles.quantityDisplay}>{count}</span>
+      <button className={styles.quantityBtn} onClick={add}>+</button>
     </div>
   );
 };
+
+// <div className="quantity-control">
+//   <button className="quantity-btn minus active">-</button>
+//   <span className="quantity-display">1</span>
+//   <button className="quantity-btn plus">+</button>
+// </div>
