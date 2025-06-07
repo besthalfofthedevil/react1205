a1675ca30075f83b457ae83df0ca721930da7af4
Aleksandr <ar@synerg.com>
2025-06-07 16:47:04 -0400

    Use store for menu

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,19 +1,12 @@
-import type { RestaurantMenuItem } from "../../mocks/restaurants.mock";
 import { MenuListItem } from "./menu-list-item";
 import styles from "./menu.module.css";
 
-export const Menu = ({ menu }: { menu: RestaurantMenuItem[] }) => {
+export const Menu = ({ menu }: { menu: string[] }) => {
   return (
     <section className={styles.menu}>
       <ul>
-        {menu.map((item) => {
-          if (!item.name) return null;
-          return <MenuListItem
-            name={item.name}
-            price={item.price}
-            ingredients={item.ingredients}
-            key={item.id}
-          />
+        {menu.map((dishId) => {
+          return <MenuListItem key={dishId} dishId={dishId} />;
         })}
       </ul>
     </section>
