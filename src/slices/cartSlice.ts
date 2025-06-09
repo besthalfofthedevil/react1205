1acf66d26509c98165982ea2e789bbec54c5367f
Aleksandr <ar@synerg.com>
2025-06-09 17:15:10 -0400

    Add cartSlice

---
+++ src/slices/cartSlice.ts
@@ -0,0 +1,49 @@
+import { createSelector, createSlice } from "@reduxjs/toolkit";
+import type { RootState } from "../store";
+
+export type CartStore = Record<string, number>;
+
+export const cartSlice = createSlice({
+  name: "cartSlice",
+  initialState: {},
+  reducers: {
+    addToCart: (state: CartStore, { payload }) => {
+      state[payload] = (state[payload] || 0) + 1;
+    },
+    removeFromCart: (state: CartStore, { payload }) => {
+      if (!state[payload]) {
+        return;
+      }
+
+      state[payload] = state[payload] - 1;
+
+      if (state[payload] === 0) {
+        delete state[payload];
+      }
+    },
+  },
+  selectors: {
+    selectItemAmountById: (state: CartStore, id) => {
+      return state[id] || 0;
+    },
+  },
+});
+
+const selectCartSlice = (state: RootState) => state[cartSlice.name];
+
+export const selectCartItems = createSelector(
+  [selectCartSlice],
+  (cartSlice: CartStore) => {
+    return Object.keys(cartSlice).reduce<{ id: string; amount: number }[]>(
+      (acc, id) => {
+        acc.push({ id, amount: cartSlice[id] });
+
+        return acc;
+      },
+      []
+    );
+  }
+);
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { selectItemAmountById } = cartSlice.selectors;
