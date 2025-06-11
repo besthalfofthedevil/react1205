78ac0ebe9a5d6d71ca54133f5466d30b5dc846a4
Aleksandr <ar@synerg.com>
2025-06-11 18:34:13 -0400

    Add local cursine selector state

--- src/components/CuisineNavigation/CuisineNavigation.tsx
+++ src/components/CuisineNavigation/CuisineNavigation.tsx
@@ -1,19 +1,65 @@
+import classNames from "classnames";
 import styles from "./CuisineNavigation.module.css";
+import { useState } from "react";
 
 export const CuisineNavigation = () => {
+  const cuisines = [
+    "pizza",
+    "sushi",
+    "vegan",
+    "steak",
+    "asian",
+    "seafood",
+    "pasta",
+  ];
+
+  const [selectedCuisines, setCursine] = useState<string[]>([]);
+  const handleAllCuisinesChange = () => {
+    setCursine((prevCuisines) =>
+      prevCuisines.length === cuisines.length ? [] : [...cuisines]
+    );
+  };
+
+  const isAllSelected = (selected: string[]) =>
+    selected.length === cuisines.length;
+
+  const handleCuisineChange = (cuisine: string) => {
+    setCursine((prevCuisines) =>
+      prevCuisines.includes(cuisine)
+        ? prevCuisines.filter((c) => c !== cuisine)
+        : [...prevCuisines, cuisine]
+    );
+  };
+
+  const isCuisineSelected = (cuisine: string) => {
+    console.log(selectedCuisines);
+    return selectedCuisines.includes(cuisine);
+  };
+
   return (
     <nav className={styles.cuisineNav}>
-        <span className={styles.cuisineLabel}>Cuisines:</span>
-        <ul className={styles.cuisineList}>
-            <li className={styles.cuisineItem}>all</li>
-            <li className={styles.cuisineItem}>pizza</li>
-            <li className={styles.cuisineItem}>sushi</li>
-            <li className={styles.cuisineItem}>vegan</li>
-            <li className={styles.cuisineItem}>steak</li>
-            <li className={styles.cuisineItem}>asian</li>
-            <li className={styles.cuisineItem}>seafood</li>
-            <li className={styles.cuisineItem}>pasta</li>
-        </ul>
+      <span className={styles.cuisineLabel}>Cuisines:</span>
+      <ul className={styles.cuisineList}>
+        <li
+          className={classNames(styles.cuisineItem, {
+            [styles.active]: isAllSelected(selectedCuisines),
+          })}
+          onClick={handleAllCuisinesChange}
+        >
+          all
+        </li>
+        {cuisines.map((cuisine) => (
+          <li
+            key={cuisine}
+            className={classNames(styles.cuisineItem, {
+              [styles.active]: isCuisineSelected(cuisine),
+            })}
+            onClick={() => handleCuisineChange(cuisine)}
+          >
+            {cuisine}
+          </li>
+        ))}
+      </ul>
     </nav>
   );
-}
+};
