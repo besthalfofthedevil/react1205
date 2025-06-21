81637e1a2917fe15081f6aac464d38e43f10a96e
Aleksandr <ar@synerg.com>
2025-06-21 09:34:24 -0400

    cd fix: remove doubled request

--- src/pages/RestaurantMenuPage/RestaurantMenuPage.tsx
+++ src/pages/RestaurantMenuPage/RestaurantMenuPage.tsx
@@ -6,11 +6,10 @@ import { Menu } from "../../components/Menu/menu";
 import { useRequest } from "../../redux/hooks/useRequest";
 import { getRestaurantMenu } from "../../redux/entities/dishes/getMenu";
 import { RequestStatus } from "../../redux/entities/request/requestSlice";
-import { getRestaurant } from "../../redux/entities/restaurants/getRestaurant";
 
 export const RestaurantMenuPage = () => {
   const { restaurantId = "" } = useParams();
-  const requestRestaurantStatus = useRequest(getRestaurant, restaurantId);
+  // const requestRestaurantStatus = useRequest(getRestaurant, restaurantId);
   const requestRestaurantMenuStatus = useRequest(
     getRestaurantMenu,
     restaurantId
@@ -21,10 +20,7 @@ export const RestaurantMenuPage = () => {
       (state: RootState) => selectRestaurantById(state, restaurantId)?.menu
     ) || [];
 
-  if (
-    requestRestaurantStatus !== RequestStatus.fulfilled &&
-    requestRestaurantMenuStatus !== RequestStatus.fulfilled
-  ) {
+  if (requestRestaurantMenuStatus !== RequestStatus.fulfilled) {
     return "Loading...";
   }
   return <Menu menu={menuIds} />;
