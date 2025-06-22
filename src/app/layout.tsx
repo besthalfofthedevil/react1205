c81b39a833ffb3cb92919ff8be55d4ce7c8d6cd8
Aleksandr <ar@synerg.com>
2025-06-22 13:27:15 -0400

    Migrate from vite to next

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
