670c65d89029879909718860d5e720616c4e5229
Aleksandr <ar@synerg.com>
2025-06-28 09:51:12 -0400

    cr fix: use router in NavigateBack

--- src/components/NavigateBack/NavigateBack.tsx
+++ src/components/NavigateBack/NavigateBack.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
-
+import { useRouter } from "next/navigation";
 
 export const NavigateBack = () => {
-  return <Link href="/" />;
+  const router = useRouter();
+  const returnBack = () => {
+    router.back();
+  };
+  return <Link href="" onClick={returnBack}/>;
 };
