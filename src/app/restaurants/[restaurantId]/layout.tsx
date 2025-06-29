bac46efed6e4cc26f140c50b94ad7f529a8ec828
Aleksandr <ar@synerg.com>
2025-06-29 07:48:35 -0400

    cr fixe: Remove wrapper

--- src/app/restaurants/[restaurantId]/layout.tsx
+++ src/app/restaurants/[restaurantId]/layout.tsx
@@ -1,9 +1,3 @@
-"use client";
-
 import { RestaurantPage } from "../../../pages/RestaurantPage/RestaurantPage";
 
-const RestrauntsLayout = ({ children }: { children: React.ReactNode }) => (
-  <RestaurantPage>{children}</RestaurantPage>
-);
-
-export default RestrauntsLayout;
+export default RestaurantPage;
